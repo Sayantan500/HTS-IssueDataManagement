0e91778763b19ea56bb7e6d1f5063cde85e60138
sayantan <sayantandas657@outlook.com>
2023-04-15 00:14:02 +0530

    Initial Commit

---
+++ src/main/java/com/helpdesk_ticketing_system/issue_data_management/IssueDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk_ticketing_system.issue_data_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueDataManagementApplication.class, args);
+	}
+
+}
